1d6de689240b9b3a6bd0b4b1b11887f27258e503
Robert Kowalski <rok@kowalski.gd>
2015-02-21 03:07:44 +0100

    incorporating feedback from mrdjules, fixing test-descriptions

--- test/get-file-ending-test.js
+++ test/get-file-ending-test.js
@@ -5,42 +5,46 @@ var assert = require('assert');
 var getFileEnding = require('./../lib/get-file-ending.js');
 
 lab.experiment('get fileendings', function() {
-  lab.test('detects .md', function(done) {
+  lab.test('returns an error for NON-existing files with defined md fileendings', function(done) {
     getFileEnding('rocko-artischocko.md', function(err, res) {
-      assert.equal(res, null);
+      var msgContains = /no file found/.test(err.message);
+      assert.ok(msgContains, 'errormessage contains "no file found"');
+      assert.ok(err, 'returns an error');
       done();
     });
   });
 
-  lab.test('detects .markdown', function(done) {
+  lab.test('returns an error for NON-existing files with defined md fileendings', function(done) {
     getFileEnding('rocko-artischocko.markdown', function(err, res) {
-      assert.equal(res, null);
+      var msgContains = /no file found/.test(err.message);
+      assert.ok(msgContains, 'errormessage contains "no file found"');
+      assert.ok(err, 'returns an error');
       done();
     });
   });
 
-  lab.test('if .markdown file exists and ending already given, returns null', function(done) {
+  lab.test('if .markdown does exist and ending already given, returns null', function(done) {
     getFileEnding(__dirname + '/fixtures/markdown/index.markdown', function(err, res) {
       assert.equal(res, null);
       done();
     });
   });
 
-  lab.test('if .md file exists and ending already given, returns null', function(done) {
+  lab.test('if .md file does exist and ending already given, returns null', function(done) {
     getFileEnding(__dirname + '/fixtures/md/index.md', function(err, res) {
       assert.equal(res, null);
       done();
     });
   });
 
-  lab.test('if .markdown file NOT exists and ending already given, returns null', function(done) {
+  lab.test('if .markdown file exists and ending NOT given, returns ending', function(done) {
     getFileEnding(__dirname + '/fixtures/markdown/index', function(err, res) {
       assert.equal(res, '.markdown');
       done();
     });
   });
 
-  lab.test('if .md file exists NOT and ending already given, returns null', function(done) {
+  lab.test('if .md file exists and ending already given, returns ending', function(done) {
     getFileEnding(__dirname + '/fixtures/md/index', function(err, res) {
       assert.equal(res, '.md');
       done();
@@ -56,7 +60,7 @@ lab.experiment('get fileendings', function() {
       });
   });
 
-  lab.test('guessing fileendings: return an error file does not exist',
+  lab.test('guessing fileendings: return an error if file does not exist',
     function(done) {
       getFileEnding(__dirname + '/doesnotexist', function(err, res) {
         var msgContains = /no file found/.test(err.message);
